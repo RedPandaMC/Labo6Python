7a47380a08bb335b7fd22eed518ee66ec6acd7f5
RedPandaMC <jurreandenys@gmail.com>
2023-03-25 21:15:37 +0100

    added main()

--- main.py
+++ main.py
@@ -11,8 +11,17 @@ def management_mode_picker():
     mm.show_functions()
     inputnum = input("Please choose a mode")
     match (inputnum):
-        case (1):
+        case ("1"):
             print("hello")
+        case ("2"):
+            print("hello")
+        case("3"):
+            print("hello")
+        case("4"):
+            print("hello")
+        case("Q"):
+            
+            
 
 #region openingurls
 #checks for dir called history,
@@ -32,31 +41,34 @@ urls = checkurls.readlines()
 for i,line in enumerate(urls):
     urls[i] = line.replace(R'\n','')
 #endregion
+def main():
+    if len(sys.argv) > 0:
+        for argument in sys.argv[1:]:
+            if url.is_string_an_url(argument):
+                if argument not in urls:
+                    listofurls.write(f"{argument}")
+                    print(f"Added website '{argument}' to list.")
+                else:
+                    print(f"{argument} is already part of the list.")
+                continue
+            match (argument):
+                case ("--manage" | "--m"):
+                    os.system("cls")
+                    print("You have chosen management mode.")
+                    for i in track(
+                        range(100),
+                        description="[#3a86ff]Loading...",
+                        style="#E5E5E5",
+                        finished_style="#FF006E",
+                    ):
+                        time.sleep(0.03)
+                    print(":)")
+                case ("--check" | "--c"):
+                    # do check
+                    print("world")
+                case (_):
+                    # skip + send "argument not recognized"
+                    print("Argument not recognized, skipping...")
 
-if len(sys.argv) > 0:
-    for argument in sys.argv[1:]:
-        if url.is_string_an_url(argument):
-            if argument not in urls:
-                listofurls.write(f"{argument}")
-                print(f"Added website '{argument}' to list.")
-            else:
-                print(f"{argument} is already part of the list.")
-            continue
-        match (argument):
-            case ("--manage" | "--m"):
-                os.system("cls")
-                print("You have chosen management mode.")
-                for i in track(
-                    range(100),
-                    description="[#3a86ff]Loading...",
-                    style="#E5E5E5",
-                    finished_style="#FF006E",
-                ):
-                    time.sleep(0.03)
-                print(":)")
-            case ("--check" | "--c"):
-                # do check
-                print("world")
-            case (_):
-                # skip + send "argument not recognized"
-                print("Argument not recognized, skipping...")
+if __name__ == "__main__":
+    main()
