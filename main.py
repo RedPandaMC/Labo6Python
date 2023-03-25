e45cef4e232ec6730ea3bd8577b0edc235c088a0
RedPandaMC <jurreandenys@gmail.com>
2023-03-25 22:24:59 +0100

    committed

--- main.py
+++ main.py
@@ -5,43 +5,79 @@ from rich.progress import track
 import urlchecker as url
 import managementmodes as mm
 
-mm.show_functions()
+def delete_new_lines(list:list):
+    newlist = []
+    for line in list:
+        newlist.append(str(line).replace(R"\n", ""))
+    return newlist
 
 def management_mode_picker():
-    mm.show_functions()
-    inputnum = input("Please choose a mode")
-    match (inputnum):
-        case ("1"):
-            print("hello")
-        case ("2"):
-            print("hello")
-        case("3"):
-            print("hello")
-        case("4"):
-            print("hello")
-        case("Q"):
-            quit("Quiting program")
-            
-            
+    while True:
+        os.system("cls")
+        mm.show_functions()
+        inputnum = input("Please choose a mode: ")
+        match (inputnum):
+            case ("1"):
+                while True:
+                    DIR = "history"
+                    urltxt = open(f"{DIR}/urls.txt", "a")
+                    check = open(f"{DIR}/urls.txt", "r")
 
-#region openingurls
-#checks for dir called history,
-#if it doesn't exist makes the dir
+                    websiteurl = input(
+                        "Give a valid url (https://www.site.com) or leave empty to exit: "
+                    )
+
+                    if websiteurl == "":
+                        break
+
+                    url_list = check.readlines()
+                    url_list_no_newlines = delete_new_lines(url_list)
+                    print(url_list_no_newlines)
+
+                    if url.is_string_an_url(websiteurl):
+                        if websiteurl not in url_list_no_newlines:
+                            urltxt.write(f"{websiteurl}\n")
+                            print(f"Added website '{websiteurl}' to list.")
+                        else:
+                            print(f"{websiteurl} is already part of the list.")
+                    else:
+                        print("That isn't a valid URL")
+                    continue
+            case ("2"):
+                urls = checkurls.readlines()
+                for i, line in enumerate(urls):
+                    urls[i] = line.replace(R"\n", "")
+                print(*urls,sep='\n')
+            case ("3"):
+                print("hello")
+            case ("4"):
+                print("hello")
+            case ("Q"):
+                quit()
+            case (_):
+                print("Not recognized, try again")
+
+
+# region openingurls
+# checks for dir called history,
+# if it doesn't exist makes the dir
 DIR = "history"
 if not os.path.exists(DIR):
     os.mkdir(DIR)
 
-#opens the list in read and append mode,
-#if the file doesn't exist it also
-#creates it in the dir
-listofurls = open(f"{DIR}/urls.json","a")
-checkurls = open(f"{DIR}/urls.json","r")
+# opens the list in read and append mode,
+# if the file doesn't exist it also
+# creates it in the dir
+listofurls = open(f"{DIR}/urls.txt", "a")
+checkurls = open(f"{DIR}/urls.txt", "r")
 urls = checkurls.readlines()
 
-#removes \n from urls
-for i,line in enumerate(urls):
-    urls[i] = line.replace(R'\n','')
-#endregion
+# removes \n from urls
+for i, line in enumerate(urls):
+    urls[i] = line.replace(R"\n", "")
+# endregion
+
+
 def main():
     if len(sys.argv) > 0:
         for argument in sys.argv[1:]:
@@ -56,20 +92,21 @@ def main():
                 case ("--manage" | "--m"):
                     os.system("cls")
                     print("You have chosen management mode.")
-                    for i in track(
+                    for _ in track(
                         range(100),
                         description="[#3a86ff]Loading...",
                         style="#E5E5E5",
                         finished_style="#FF006E",
                     ):
                         time.sleep(0.03)
-                    print(":)")
+                    management_mode_picker()
                 case ("--check" | "--c"):
                     # do check
                     print("world")
                 case (_):
                     # skip + send "argument not recognized"
                     print("Argument not recognized, skipping...")
+
 
 if __name__ == "__main__":
     main()
