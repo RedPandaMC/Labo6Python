1673ae76f3338684f87b11a42918a23ff7c37cd3
RedPandaMC <jurreandenys@gmail.com>
2023-03-26 13:57:19 +0200

    deleted bugs; made code pretty

--- main.py
+++ main.py
@@ -5,11 +5,30 @@ from rich.progress import track
 import urlchecker as url
 import managementmodes as mm
 
-def delete_new_lines(list:list):
+# region openingurls
+# checks for dir called history,
+# if it doesn't exist makes the dir
+DIR = "history"
+if not os.path.exists(DIR):
+    os.mkdir(DIR)
+# open documents to be used later
+urltext = open(f"{DIR}/urls.txt", "a",encoding="UTF-8")
+checkurls = open(f"{DIR}/urls.txt", "r",encoding="UTF-8")
+# endregion
+
+def delete_new_lines(ls_url: list) -> list:
     newlist = []
-    for line in list:
-        newlist.append(str(line).replace(R"\n", ""))
+    for line in ls_url:
+        newlist.append(line.replace("\n", ""))
     return newlist
+
+def is_website_url_already_given(url_to_check:str):
+    check = open(f"{DIR}/urls.txt", "r",encoding="UTF-8")
+    url_list = delete_new_lines(check.readlines())
+    if url_to_check not in url_list:
+        return True
+    return False
+
 
 def management_mode_picker():
     while True:
@@ -19,10 +38,7 @@ def management_mode_picker():
         match (inputnum):
             case ("1"):
                 while True:
-                    DIR = "history"
-                    urltxt = open(f"{DIR}/urls.txt", "a")
-                    check = open(f"{DIR}/urls.txt", "r")
-
+                    urltxt = open(f"{DIR}/urls.txt", "a",encoding="UTF-8")
                     websiteurl = input(
                         "Give a valid url (https://www.site.com) or leave empty to exit: "
                     )
@@ -30,24 +46,19 @@ def management_mode_picker():
                     if websiteurl == "":
                         break
 
-                    url_list = check.readlines()
-                    url_list_no_newlines = delete_new_lines(url_list)
-                    print(url_list_no_newlines)
-
                     if url.is_string_an_url(websiteurl):
-                        if websiteurl not in url_list_no_newlines:
+                        if is_website_url_already_given(websiteurl):
                             urltxt.write(f"{websiteurl}\n")
                             print(f"Added website '{websiteurl}' to list.")
                         else:
-                            print(f"{websiteurl} is already part of the list.")
+                            print(f"'{websiteurl}' is already part of the list.")
                     else:
                         print("That isn't a valid URL")
                     continue
             case ("2"):
-                urls = checkurls.readlines()
-                for i, line in enumerate(urls):
-                    urls[i] = line.replace(R"\n", "")
+                urls = delete_new_lines(checkurls.readlines())
                 print(*urls,sep='\n')
+                input("Press enter to continue.")
             case ("3"):
                 print("hello")
             case ("4"):
@@ -58,46 +69,26 @@ def management_mode_picker():
                 print("Not recognized, try again")
 
 
-# region openingurls
-# checks for dir called history,
-# if it doesn't exist makes the dir
-DIR = "history"
-if not os.path.exists(DIR):
-    os.mkdir(DIR)
 
-# opens the list in read and append mode,
-# if the file doesn't exist it also
-# creates it in the dir
-listofurls = open(f"{DIR}/urls.txt", "a")
-checkurls = open(f"{DIR}/urls.txt", "r")
-urls = checkurls.readlines()
-
-# removes \n from urls
-for i, line in enumerate(urls):
-    urls[i] = line.replace(R"\n", "")
-# endregion
 
 
 def main():
     if len(sys.argv) > 0:
         for argument in sys.argv[1:]:
             if url.is_string_an_url(argument):
-                if argument not in urls:
-                    listofurls.write(f"{argument}")
+                if is_website_url_already_given(argument):
+                    urltext.write(f"{argument}")
                     print(f"Added website '{argument}' to list.")
                 else:
                     print(f"{argument} is already part of the list.")
                 continue
+
             match (argument):
                 case ("--manage" | "--m"):
                     os.system("cls")
                     print("You have chosen management mode.")
-                    for _ in track(
-                        range(100),
-                        description="[#3a86ff]Loading...",
-                        style="#E5E5E5",
-                        finished_style="#FF006E",
-                    ):
+                    for _ in track(range(100),description="[#3a86ff]Loading...",style="#E5E5E5",
+                                   finished_style="#FF006E",):
                         time.sleep(0.03)
                     management_mode_picker()
                 case ("--check" | "--c"):
@@ -109,4 +100,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
