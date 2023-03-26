2ebf5fad4b82f109d319c6c06a44d242c5cf35dd
RedPandaMC <jurreandenys@gmail.com>
2023-03-26 15:43:57 +0200

    Added explainations to all functions

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+This code is used to ping a list of websites \n
+to check if they are online or not
+"""
 import time
 import sys
 import os
@@ -5,49 +9,68 @@ from rich.progress import track
 import urlchecker as url
 import managementmodes as mm
 
-def delete_new_lines(ls_url: list) -> list:
+def delete_new_lines(ls_url: list):
+    """
+    This function gives the user a list without '\\n'
+    """
     newlist = []
     for line in ls_url:
         newlist.append(line.replace("\n", ""))
     return newlist
 
 def is_website_url_already_given(url_to_check:str):
+    """
+    This function checks if the given item is already in urls.txt
+    """
     check = open(f"{DIR}/urls.txt", "r",encoding="UTF-8")
     url_list = delete_new_lines(check.readlines())
     if url_to_check not in url_list:
         return True
     return False
 
+def add_websites():
+    """
+    This function allows you to add websites to urls.txt,\n
+    also checks if the given item is already in urls.txt
+    """
+    while True:
+        urltxt = open(f"{DIR}/urls.txt", "a",encoding="UTF-8")
+        websiteurl = input("Give a valid url (https://www.site.com) or leave empty to exit: ")
+        if websiteurl == "":
+            break
+        if url.is_string_an_url(websiteurl):
+            if is_website_url_already_given(websiteurl):
+                urltxt.write(f"{websiteurl}\n")
+                print(f"Added website '{websiteurl}' to list.")
+            else:
+                print(f"'{websiteurl}' is already part of the list.")
+        else:
+            print("That isn't a valid URL")
+        continue
+
+def view_websites():
+    """
+    This function allows you to view all the websites in urls.txt
+    """
+    urls = delete_new_lines(checkurls.readlines())
+    print(*urls,sep='\n')
+    input("Press enter to continue.")
+
+
 
 def management_mode_picker():
+    """
+    This function is used to let the user pick a mode
+    """
     while True:
         os.system("cls")
         mm.show_functions()
         inputnum = input("Please choose a mode: ")
         match (inputnum):
             case ("1"):
-                while True:
-                    urltxt = open(f"{DIR}/urls.txt", "a",encoding="UTF-8")
-                    websiteurl = input(
-                        "Give a valid url (https://www.site.com) or leave empty to exit: "
-                    )
-
-                    if websiteurl == "":
-                        break
-
-                    if url.is_string_an_url(websiteurl):
-                        if is_website_url_already_given(websiteurl):
-                            urltxt.write(f"{websiteurl}\n")
-                            print(f"Added website '{websiteurl}' to list.")
-                        else:
-                            print(f"'{websiteurl}' is already part of the list.")
-                    else:
-                        print("That isn't a valid URL")
-                    continue
+                add_websites()
             case ("2"):
-                urls = delete_new_lines(checkurls.readlines())
-                print(*urls,sep='\n')
-                input("Press enter to continue.")
+                view_websites()
             case ("3"):
                 print("hello")
             case ("4"):
@@ -58,6 +81,9 @@ def management_mode_picker():
                 print("Not recognized, try again")
 
 def main():
+    """
+    This function is used to interpret the given arguments
+    """
     if len(sys.argv) > 0:
         for argument in sys.argv[1:]:
             if url.is_string_an_url(argument):
