17c864e84905c0e7a21c074528ff949bdca79349
RedPandaMC <jurreandenys@gmail.com>
2023-03-27 19:13:57 +0200

    Added a 0 min spam prevention

--- scheduledpings.py
+++ scheduledpings.py
@@ -7,6 +7,8 @@ readschedule = open(f"{dir}/schedule.json","r",encoding="UTF-8")
 settings = json.load(readschedule)
 sec = 0.0
 while settings["do-scheduled-ping"]:
+    if (settings["schedule-time-in-minutes"]*60) < 2.0:
+        break
     if sec >= (settings["schedule-time-in-minutes"]*60):
         mm.perform_ping_check()
         sec = 0.0
